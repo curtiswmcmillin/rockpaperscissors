29e2e1a6a55e4c3f85defe215fc08d2e26a8685f
CURTIS MCMILLIN <curtismcmillin@CURTISs-iMac.local>
2016-06-10 17:22:43 -0500

    making the opponent dynamic

--- rockpaperscissors/rockpaperscissors/ResultsViewController.swift
+++ rockpaperscissors/rockpaperscissors/ResultsViewController.swift
@@ -12,11 +12,66 @@ class ResultsViewController:UIViewController {
     
     var selectedItem:String!
     @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private weak var opponentLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        resultLabel.text = self.selectedItem
+        resultLabel.text = compareToOpponent()
+        
+    }
+    
+    private func compareToOpponent() -> String {
+        let opponent = generateRandomOpponent()
+        print("opponent was \(opponent)")
+        opponentLabel.text = opponent
+        switch selectedItem {
+            case "Rock - Code Only":
+                switch opponent {
+                    case "Paper":
+                        return "Paper Covers Rock, You Lose."
+                    case "Rock":
+                        return "It's a Tie!"
+                    case "Scissors":
+                        return "Rock Dulls Scissors, You Win!"
+                    default:
+                        return "Not sure what happend!?"
+
+            }
+            case "Paper - Code and Segue":
+                switch opponent {
+                    case "Paper":
+                        return "It's a Tie!"
+                    case "Rock":
+                        return "Paper Covers Rock, You Win!"
+                    case "Scissors":
+                        return "Scissors Cut Paper, You Lose."
+                    default:
+                        return "Not sure what happend!?"
+
+            }
+            case "Scissors - Segue Only":
+                switch opponent {
+                    case "Paper":
+                        return "Scissors Cut Paper, You Win!"
+                    case "Rock":
+                        return "Rock Dulls Scissors, You Lose."
+                    case "Scissors":
+                        return "It's a Tie!"
+                    default:
+                        return "Not sure what happend!?"
+                    
+            }
+            default:
+                return "Not sure what happend!?"
+        }
+    }
+    
+    private func generateRandomOpponent() -> String {
+        
+        let shapes = ["Rock", "Paper", "Scissors"]
+        let randomChoice = shapes[Int(arc4random_uniform(3))]
+        return randomChoice
     }
     
     override func didReceiveMemoryWarning() {
